3668e8850da665ba73a851005f689fc12e0c1f74
Parkduksung <duksung1234@naver.com>
2021-02-11 01:15:07 +0900

    [LifeCycle-Room] Thread 로 작업한 부분 코루틴 적용.

--- app/src/main/java/com/work/studyaac/data/source/remote/LifeCycleLocalDataSourceImpl.kt
+++ app/src/main/java/com/work/studyaac/data/source/remote/LifeCycleLocalDataSourceImpl.kt
@@ -4,18 +4,25 @@ import android.util.Log
 import com.work.studyaac.data.model.Person
 import com.work.studyaac.network.room.database.PersonDatabase
 import com.work.studyaac.network.room.entity.PersonEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LifeCycleLocalDataSourceImpl(private val personDatabase: PersonDatabase) :
     LifeCycleLocalDataSource {
     override fun createPerson(person: Person) {
-        Thread {
+        CoroutineScope(Dispatchers.IO).launch {
             personDatabase.personDao().registerPerson(
                 PersonEntity(
                     personName = person.personName,
                     personAge = person.personAge
                 )
             )
-        }.start()
-        Log.d("결과", "여기찍힘")
+
+            withContext(Dispatchers.Main) {
+                Log.d("결과", "여기찍힘")
+            }
+        }
     }
 }
